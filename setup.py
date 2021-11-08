d0f3e6c61a3cb860d743fd12e86e346c3cf09b30
James Ansley <ansleyjpf@gmail.com>
2021-11-09 10:35:48 +1300

    init commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cowexcept',
+    version='0.1.dev1',
+    author='James Finnie-Ansley',
+    description='Spice up those exceptions with cowexcept!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=setuptools.find_packages(
+        where='src',
+    ),
+    package_dir={'': 'src'},
+    python_requires='>=3.8',
+    install_requires=[
+        'cowpy',
+    ],
+)
