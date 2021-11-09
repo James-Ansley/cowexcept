f1ab4d2c4c32df034a21ca160ec89c524a827201
James Ansley <ansleyjpf@gmail.com>
2021-11-09 19:07:04 +1300

    V1.2 with Improved Cow
    Cow is now more similar to original cowsay cow

--- src/cowexcept/__init__.py
+++ src/cowexcept/__init__.py
@@ -1,12 +1,22 @@
-import traceback as _traceback
-from cowpy import cow as _cow
 import sys as _sys
+import traceback as _traceback
+
+from cowpy.cow import Cowacter as _Cowacter
 
 
 def _cowsay_except(type, value, tracebac):
+    class Default(_Cowacter):
+        def __init__(self, **kwargs):
+            kwargs['body'] = ("     {thoughts}   ^__^\n"
+                              "      {thoughts}  ({eyes})\\_______\n"
+                              "         (__)\\       )\\/\\\n"
+                              "          {tongue}   ||----w |\n"
+                              "             ||     ||")
+            super().__init__(**kwargs)
+
     print('Traceback (most recent call last):', file=_sys.stderr)
     _traceback.print_tb(tracebac, file=_sys.stderr)
-    print(_cow.Cowacter().milk(f'{type.__name__}: {value}'), file=_sys.stderr)
+    print(Default().milk(f'{type.__name__}: {value}'), file=_sys.stderr)
 
 
 _sys.excepthook = _cowsay_except
