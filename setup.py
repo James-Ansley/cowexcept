6e575983e89f41025b47d44bf7707694361af6fa
James Ansley <james@ansley.dev>
2021-11-09 19:07:04 +1300

    V1.2 with Improved Cow
    Cow is now more similar to original cowsay cow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='cowexcept',
-    version='1.0',
+    version='1.2',
     author='James Finnie-Ansley',
     description='Spice up those exceptions with cowexcept!',
     long_description=long_description,
