e865ff149bfeb34cb7197ac795590e88662ca2e4
James Ansley <ansleyjpf@gmail.com>
2022-02-17 01:46:36 +1300

    changed to python-cowsay

--- src/cowexcept/__init__.py
+++ src/cowexcept/__init__.py
@@ -1,22 +1,18 @@
 import sys as _sys
-import traceback as _traceback
+from io import StringIO as _StringIO
 
-from cowpy.cow import Cowacter as _Cowacter
+from cowsay import cowsay as _cowsay
 
 
 def _cowsay_except(type, value, tracebac):
-    class Default(_Cowacter):
-        def __init__(self, **kwargs):
-            kwargs['body'] = ("     {thoughts}   ^__^\n"
-                              "      {thoughts}  ({eyes})\\_______\n"
-                              "         (__)\\       )\\/\\\n"
-                              "          {tongue}   ||----w |\n"
-                              "             ||     ||")
-            super().__init__(**kwargs)
-
-    print('Traceback (most recent call last):', file=_sys.stderr)
-    _traceback.print_tb(tracebac, file=_sys.stderr)
-    print(Default().milk(f'{type.__name__}: {value}'), file=_sys.stderr)
+    _sys.stderr = error = _StringIO()
+    _sys.__excepthook__(type, value, tracebac)
+    _sys.stderr = _sys.__stderr__
+    cow = _cowsay(
+        error.getvalue(),
+        wrap_text=False,
+    )
+    print(cow, file=_sys.stderr)
 
 
 _sys.excepthook = _cowsay_except
